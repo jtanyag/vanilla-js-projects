ea26bcbde2f32dd98d5e93d473e2325f866e4784
jtanyag <jtanyag@gmail.com>
2021-10-15 23:16:35 -0700

    WIP creates review-carousel

---
+++ review-carousel/app.js
@@ -0,0 +1,30 @@
+const reviews = [
+  {
+    id: 1,
+    img: "",
+    name: "John Doe",
+    job: "Front End Developer",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, distinctio illum quis nobis aperiam mollitia non exercitationem alias reprehenderit similique tempore, maiores quaerat nemo debitis aut."
+  },
+  {
+    id: 2,
+    img: "",
+    name: "Jane Doe",
+    job: "Back End Developer",
+    text: "Doggo ipsum extremely cuuuuuute length boy pupperino extremely cuuuuuute sub woofer, shoober aqua doggo fat boi. Corgo heck thicc blep doggo, heckin good boys I am bekom fat wow such tempt doggorino boofers, aqua doggo he made many woofs mlem. very good spot porgo. Shooberino much ruin diet big ol heck vvv very jealous pupper, waggy wags heckin angery woofer length boy. Shoober corgo many pats puggo, length boy doggorino."
+  },
+  {
+    id: 3,
+    img: "",
+    name: "John Name",
+    job: "Full Stack Developer",
+    text: " Heckin good boys thicc aqua doggo puggo porgo doge shoober adorable doggo you are doin me a concern, snoot yapper wow very biscit fat boi most angery pupper I have ever seen much ruin diet the neighborhood pupper. waggy wags blop much ruin diet. Vvv heckin good boys ur givin me a spook pupper, the neighborhood pupper heckin. Ur givin me a spook clouds pupper adorable doggo doggorino, lotsa pats thicc."
+  },
+  {
+    id: 4,
+    img: "",
+    name: "Joe Name",
+    job: "Software Engineer",
+    text: "Doggo ipsum you are doin me a concern smol borking doggo with a long snoot for pats puggorino doggo porgo wrinkler puggorino porgo, long bois length boy wow such tempt h*ck snoot. You are doing me a frighten noodle horse pupper wrinkler doggorino, most angery pupper I have ever seen many pats smol. Doge extremely cuuuuuute heckin good boys and girls much ruin diet h*ck long water shoob, shibe adorable doggo the neighborhood pupper I am bekom fat long water shoob, big ol very taste wow wow very biscit sub woofer."
+  }
+];
